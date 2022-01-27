6c16fd8f70d9bcc01f9bebd88ed0dc753175bb55
Calvin Job Puram <puram.calvin@gmail.com>
2022-01-27 08:22:29 +0100

    add startup script

--- app.js
+++ app.js
@@ -8,5 +8,5 @@ app.get("/", (req, res) => {
 const port = process.env.PORT || 4000;
 
 app.listen(port, () => {
-  console.log(`server listening on port ${port}`);
+  console.log(`server listening on port ${port}!!!`);
 });
